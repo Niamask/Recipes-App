d5efb946d1b9e72d1fc7c9954897ed9cbeda21ca
Niamask <niamaskay@gmail.com>
2024-01-20 22:01:18 +0100

    Developing a DOM Update Algorithm

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -21,12 +21,16 @@ class ResultsView extends View {
   }
 
   _generateMarkupPreview(result) {
-    // const id = window.location.hash.slice(1);
+    const id = window.location.hash.slice(1);
 
     return `<li class="preview">
-                <a class="preview__link" href="#${result.id}">
+                <a class="preview__link ${
+                  result.id === id ? "preview__link--active" : ""
+                }" href="#${result.id}">
                   <figure class="preview__fig">
-                    <img  crossorigin="anonymous" src="${result.image}" alt="${result.title}" />
+                    <img  crossorigin="anonymous" src="${result.image}" alt="${
+      result.title
+    }" />
                   </figure>
                   <div class="preview__data">
                     <h4 class="preview__title">${result.title}</h4>
