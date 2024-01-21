74f17bdcb56a4c32d8b387addd9f32ec73d44a6b
Niamask <niamaskay@gmail.com>
2024-01-21 18:06:16 +0100

    final update

--- src/js/controller.js
+++ src/js/controller.js
@@ -134,6 +134,10 @@ const controlAddRecipe = async function (newRecipe) {
   // }
 };
 
+const newFeature = function () {
+  console.log("welcome to the application!");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -142,5 +146,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
